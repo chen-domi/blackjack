50281bac1a6ad328af26b33df29afd074d2336e4
Dominic Chen <cchendominic@gmail.com>
2025-10-08 20:53:39 -0400

    Use an array and a loop to cycle through the cards

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = 10 // hardcoded value
+let secondCard = 4 // hardcoded value
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -9,22 +10,32 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
-    cardsEl.textContent = "Cards " + firstCard + ", " + secondCard
-    sumEl.textContent = "Sum: " + sum
-
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
-    console.log("start button clicked")
+  renderGame()
 }
+
+function renderGame() {
+  cardsEl.textContent = "Cards: "
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " "
+  }
+
+  sumEl.textContent = "Sum: " + sum
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+  messageEl.textContent = message
+}
+
 function newCard() {
-    console.log("Drawing a new card from the deck!")
-    
+  let card = 9 // hardcoded value
+  sum += card
+  cards.push(card)
+  renderGame()
 }
